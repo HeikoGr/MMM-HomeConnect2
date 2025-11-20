bf5e61681b4e9d0fb6f6be3f854d565ba7933b8b
Heiko Gr <20295490+HeikoGr@users.noreply.github.com>
2025-11-20 16:29:57 +0000

    feat: Update ESLint and Prettier configurations; add new prettier.config.cjs

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 100,
+    singleQuote: false,
+    trailingComma: "none",
+    overrides: [
+        {
+            files: ["translations/*.json"],
+            options: {
+                printWidth: 80
+            }
+        }
+    ]
+};
